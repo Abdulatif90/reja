f2991e390b238ed4b9cef20cb617bc5bace2bc90
Sharipov Abdulatif <138142557+Abdulatif90@users.noreply.github.com>
2025-02-19 23:49:19 +0900

    upgrade

---
+++ server.js
@@ -0,0 +1,35 @@
+console.log("Web serverni boshlash");
+const express = require('express');
+const app = express();
+const http =require ('http');
+
+// 1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// 2  Session code
+
+
+// 3  backend da frontend yasaymiz ejs ichida
+app.set("views","views");
+app.set("view engine", "ejs");
+
+// 4  Routing codes
+app.post("/create-item", (req , res)=>{
+    console.log(req.body);
+    res.json({ test : "success" });
+});
+
+app.get("/", (req,res) =>{
+res.render("harid");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function(){
+    console.log(`The server is running successfully on port : ${PORT}`);
+});
+
+
