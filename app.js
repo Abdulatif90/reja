d29bdc1cecb1e68d0769e1cc4b82710db6e868ea
Sharipov Abdulatif <138142557+Abdulatif90@users.noreply.github.com>
2025-03-02 17:08:26 +0900

    feat loyihamizning yakuniy bosqich  logic

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
-const http = require("http");
-const fs = require("fs");
+const express = require("express");  // Express kutubxonasini chaqirish, serverni yaratish uchun ishlatiladi, Express- serverni yaratish uchun ishlatiladi.
+const app = express();  //app - serverni yaratish uchun ishlatiladi
+const http = require("http");  // http- serverni yaratish uchun ishlatiladi.
+const fs = require("fs"); // fs- fayllarni yaratish, o'qish va yozish uchun ishlatiladi.
 
 let user;
+// user.json faylini o'qish
+// fs.readFile- faylni o'qish uchun ishlatiladi.
+// "database/user.json"- fayl nomi
+// "utf8"- faylning kodirovkasi
+// (err, data)- xatolik va ma'lumotlar, agar xatolik bo'lsa "ERROR:", err chiqadi, aks holda user= JSON.parse(data) chiqadi.
+// JSON.parse(data)- JSON ma'lumotlarini o'qish, parse- JSON ma'lumotlarini o'qish uchun ishlatiladi.
+
 fs.readFile("database/user.json", "utf8", (err, data) => {
   if (err) {
     console.log("ERROR:", err);
@@ -19,16 +26,32 @@ const mongodb = require("mongodb");
 
 
 // 1: Kirish code
+// Static fayllar - Express serveriga public papkani ulash uchun ishlatiladi. Public- fayllarni serverga ulash uchun ishlatiladi.
+// JSON fayllar - Express serveriga JSON fayllarni ulash uchun ishlatiladi.
+// Views - Express serveriga views papkani ulash uchun ishlatiladi.
+// Routing - Express serveriga routing qilish uchun ishlatiladi.
+//urlencoded- Express serveriga urlencoded ma'lumotlarni ulash uchun ishlatiladi. urlcoded bu ma'lumotlarni kodlash uchun ishlatiladi.
+
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // 2: Session code
 // 3: Views code
+
+// app.set- Express serveriga papka va fayllarni ulash uchun ishlatiladi.
+// set() - Express serveriga papka va fayllarni ulash uchun ishlatiladi. 1 - papka nomi, 2 - papka manzili, 3 - fayl nomi, 4 - fayl manzili.
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
+
+//post- Express serveriga post ma'lumotlarni ulash uchun ishlatiladi. req- ma'lumotlarni olish uchun ishlatiladi, res- ma'lumotlarni yuborish uchun ishlatiladi.
+// req.body- ma'lumotlarni olish uchun ishlatiladi.
+// insertOne- ma'lumotlarni yuborish uchun ishlatiladi.
+ 
+
   app.post("/create-item", (req, res) => {
     console.log(req.body);
     const new_reja=req.body.reja;
@@ -47,6 +70,26 @@ app.set("view engine", "ejs");
     );
   });
 
+  app.post("/edit-item", (req, res) => {
+    const data = req.body; 
+    db.collection("plans").findOneAndUpdate(
+      {_id: new mongodb.ObjectId(data.id)},
+      {$set: {reja: data.text}},
+      function (err, data) {  
+        res.json({state: "Updated Successfully"});
+      }
+    )});
+
+  app.post("/delete-all", (req, res) => {
+    if (req.body.delete_all) {
+    db.collection("plans").deleteMany({}, (err, data) => {
+       res.json({state: "All plans are deleted"});
+      });
+    };
+  }
+  );
+
+
   app.get("/", function (req, res) {
     db.collection("plans")
     .find()
@@ -55,9 +98,11 @@ app.set("view engine", "ejs");
         console.log(err);
       } else {
         console.log(data);
+        // render  - 
         res.render("reja", {items: data});
       }
     });
   });
 
   module.exports = app;
+ 
--- public/browser.js
+++ public/browser.js
@@ -1,6 +1,10 @@
 
 console.log("Browser ishga tushdi");
-
+// itemTemplate- serverdan kelgan ma'lumotlarni ko'rsatish uchun ishlatiladi.
+// item- serverdan kelgan ma'lumotlarni o'z ichiga oladi.
+// item.reja- serverdan kelgan ma'lumotlarni o'z ichiga oladi.
+// item._id- serverdan kelgan ma'lumotlarni o'z ichiga oladi.
+   
 function itemTemplate(item){
     return ` <li
           class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
@@ -16,30 +20,36 @@ function itemTemplate(item){
           </li> `;
 }
 
-let createField= document.getElementById("create-field");
+let createField= document.getElementById("create-field"); // create-field- inputni id sini olish.
 
 document.getElementById("create-form").addEventListener("submit", function(e){
-	e.preventDefault();
+	e.preventDefault(); // submitni o'chirish uchun preventDefault() funksiyasini chaqirish, yani boshqa sahifaga o'tmaslik uchun ishlatiladi.
    
     axios
-        .post("/create-item", { reja: createField.value })
+        .post("/create-item", { reja: createField.value })  // create-item- serverdagi route nomi, {reja: createField.value}- serverga yuboriladigan ma'lumot
         .then((response) => {
-           console.log(response);
-            console.log(createField);
-            document.getElementById("item-list")
-            .insertAdjacentHTML("beforeend", itemTemplate(response.data));
-        createField.value="";
-             createField.focus();   
+            console.log(response); // response- serverdan kelgan javobni ko'rsatadi
+            console.log(createField);   // createField- inputdagi qiymatni ko'rsatadi
+            document.getElementById("item-list")    // item-list- itemlarni ko'rsatish uchun ishlatiladi
+            .insertAdjacentHTML("beforeend", itemTemplate(response.data));  // itemTemplate funksiyasini chaqirish, response.data ni o'z ichiga oladi. beforeend- item-listning oxiriga qo'shish
+        createField.value="";  // qiymat kiritgandan keyin inputdagi yozuv, bo`sh qilish uchun ishlatiladi. Agar ""  ichiga qiymat yozilsa, inputni qiymati bo`sh qilinadi.
+        createField.focus();  // inputga fokuslanish uchun ishlatiladi.   
     })
         .catch((err)=>{
             console.log("Iltimos qaytadan haratkat qiling!!!");
     });
 });
 
+ 
+
 document.addEventListener("click", function(e){
     //delete 
-    if(e.target.classList.contains("delete-me")){
-        if (confirm("Aniq o'chirasizmi?")){
+    console.log(e);
+
+    // e.target- bosilgan elementni ko'rsatadi, classList - elementning classlarini ko'rsatadi, contains- elementda class bor yoki yo'qligini tekshiradi, delete-me- class nomi
+    //confirmni o'chirish- pageda o'chirishni tasdiqlash ko`rsatiilmasligi uchun ishlatiladi.
+    if(e.target.classList.contains("delete-me")){    
+         if (confirm) {  
             axios
             .post("/delete-item", { id: e.target.getAttribute("data-id")})
             .then((response) => {
@@ -51,4 +61,52 @@ document.addEventListener("click", function(e){
             });
         }
     }
-})
+
+
+
+// edit operation
+
+    // edit-me- class nomi
+    // prompt- o'zgartirish uchun ishlatiladi va foydalanuvchidan qiymat kiritishni so'raydi. 
+    //getAttribute- elementning attributini olish uchun ishlatiladi.
+    //innerText- elementning matnini olish uchun ishlatiladi. 
+
+
+    if(e.target.classList.contains("edit-me")){
+        let userInput = prompt("o'zgartirish  kiriting", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+        if(userInput){
+            axios
+            .post("/edit-item", { text: userInput, id: e.target.getAttribute("data-id")})
+            .then(() => {
+                e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
+            })
+            .catch(() => {
+                console.log("Iltimos qaytadan harakat qiling!!!");
+            });
+        }
+    }
+});
+
+// delete all
+// clean-all- class nomi
+// addEventListener- elementga hodisani qo'shish uchun ishlatiladi.
+// axios- serverga ma'lumot yuborish uchun ishlatiladi.
+// post- serverga ma'lumot yuborish uchun ishlatiladi.
+// delete-all- serverdagi route nomi
+// delete_all- serverga yuboriladigan ma'lumot 
+// then- serverdan kelgan javobni ko'rsatish uchun ishlatiladi.
+// alert- xabarni ko'rsatish uchun ishlatiladi.
+// document.location.reload()- sahifani qayta yuklash uchun ishlatiladi.
+    
+
+document.getElementById("clean-all").addEventListener("click", function(){
+    axios
+    .post("/delete-all",{delete_all: true})
+    .then((response) => {
+        // alert(response.data.state);
+        document.location.reload();
+    });
+}); 
+ 
+
+
